515bb5532b9835c355c01d14d6db9ff0487b4bba
Vlad <vladgavilov2000@gmail.com>
2022-02-07 06:27:48 -0800

    Add project files.

---
+++ Calculator1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Calculator1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "2+1";
+            Calculator calc = new Calculator();
+
+            Console.WriteLine(calc.Add(str));
+
+        }
+    }
+}
