17391dae66c16fe46aeaf6be2dc365c43b6212a8
Vlad <vladgavilov2000@gmail.com>
2022-02-17 16:30:09 +0200

    use readonly in ConsoleApp.cs

--- Calculator.ConsoleApplication/ConsoleApp.cs
+++ Calculator.ConsoleApplication/ConsoleApp.cs
@@ -4,16 +4,14 @@ namespace Calculator.ConsoleApplication
 {
     public class ConsoleApp
     {
-        private readonly string FirstMessage;
-        private readonly string ChoiceMessage;        
-        private StringCalculator _calculate;
-        private ConsoleService _serviceConsole;
+        private const string FirstMessage = "Enter comma separated numbers";
+        private const string ChoiceMessage = "Press esc to exit or enter to continue";        
+        private readonly StringCalculator _calculate;
+        private readonly ConsoleService _serviceConsole;
 
 
         public ConsoleApp(ConsoleService consoleService, StringCalculator calculator)
         {
-            FirstMessage = "Enter comma separated numbers";
-            ChoiceMessage = "Press esc to exit or enter to continue";
             _serviceConsole = consoleService;
             _calculate = calculator;
         }
